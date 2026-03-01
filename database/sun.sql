fb6a122b1ab167edd62c2f6e46b3b5095c17e46c
Daniel Berg <daniel.berg031@gmail.com>
2026-03-01 17:49:43 -0300

    chore: create customer lead relationship

--- database/sun.sql
+++ database/sun.sql
@@ -16,4 +16,21 @@ CREATE TABLE customer (
     CONSTRAINT unique_customer_email UNIQUE (email)
 );
 
+CREATE TABLE customer_lead (
+    customer_id INTEGER NOT NULL,
+    lead_id INTEGER NOT NULL,
+    data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (customer_id, lead_id),
+
+    CONSTRAINT fk_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES customer(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_lead
+        FOREIGN KEY (lead_id)
+        REFERENCES lead(id)
+        ON DELETE CASCADE
+);
 
