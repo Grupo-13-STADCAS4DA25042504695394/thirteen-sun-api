41722c45dc0beedfc6d28d8d7778f9847a33865f
Daniel Berg <daniel.berg031@gmail.com>
2026-03-01 17:46:31 -0300

    chore: create lead table

---
+++ database/sun.sql
@@ -0,0 +1,11 @@
+CREATE TABLE lead (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    valor_medio_mes NUMERIC(10,2),
+    cep VARCHAR(10),
+    email VARCHAR(150) NOT NULL,
+    telefone VARCHAR(20),
+    CONSTRAINT unique_lead_email UNIQUE (email)
+);
+
+
