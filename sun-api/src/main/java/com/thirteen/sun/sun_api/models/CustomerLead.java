c4d1d3b5986df0e689ef22d6ec6dbb329f1cbdf2
Daniel Berg <daniel.berg031@gmail.com>
2026-03-02 20:22:25 -0300

    chore: set as entity jakarta

--- sun-api/src/main/java/com/thirteen/sun/sun_api/models/CustomerLead.java
+++ sun-api/src/main/java/com/thirteen/sun/sun_api/models/CustomerLead.java
@@ -1,5 +1,10 @@
 package com.thirteen.sun.sun_api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer_lead")
 public class CustomerLead {
 
 }
