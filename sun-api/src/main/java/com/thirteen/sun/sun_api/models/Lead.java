82481bd65d528173ef0c958ee5b27031522ba933
Daniel Berg <daniel.berg031@gmail.com>
2026-03-02 20:20:21 -0300

    chore: create constructors and getter and setters

--- sun-api/src/main/java/com/thirteen/sun/sun_api/models/Lead.java
+++ sun-api/src/main/java/com/thirteen/sun/sun_api/models/Lead.java
@@ -30,4 +30,74 @@ public class Lead {
 
     @OneToMany(mappedBy = "lead", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CustomerLead> customerLeads;
+
+    public Lead() {
+    }
+
+    public Lead(Long id, String nome, Integer perfilDeConsumo, String cep, String email, String telefone, Set<CustomerLead> customerLeads) {
+        this.id = id;
+        this.nome = nome;
+        this.perfilDeConsumo = perfilDeConsumo;
+        this.cep = cep;
+        this.email = email;
+        this.telefone = telefone;
+        this.customerLeads = customerLeads;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPerfilDeConsumo() {
+        return perfilDeConsumo;
+    }
+
+    public void setPerfilDeConsumo(Integer perfilDeConsumo) {
+        this.perfilDeConsumo = perfilDeConsumo;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Set<CustomerLead> getCustomerLeads() {
+        return customerLeads;
+    }
+
+    public void setCustomerLeads(Set<CustomerLead> customerLeads) {
+        this.customerLeads = customerLeads;
+    }
+
 }
