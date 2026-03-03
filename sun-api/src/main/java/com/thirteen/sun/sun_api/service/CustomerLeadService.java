ccd85df947c70488cdcc537fa6be3abc689361f4
Daniel Berg <daniel.berg031@gmail.com>
2026-03-03 20:40:30 -0300

    chore: create customer lead service

---
+++ sun-api/src/main/java/com/thirteen/sun/sun_api/service/CustomerLeadService.java
@@ -0,0 +1,54 @@
+package com.thirteen.sun.sun_api.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.thirteen.sun.sun_api.models.Customer;
+import com.thirteen.sun.sun_api.models.CustomerLead;
+import com.thirteen.sun.sun_api.models.CustomerLeadId;
+import com.thirteen.sun.sun_api.models.Lead;
+import com.thirteen.sun.sun_api.repository.CustomerLeadRepository;
+import com.thirteen.sun.sun_api.repository.CustomerRepository;
+import com.thirteen.sun.sun_api.repository.LeadRepository;
+
+import java.time.LocalDateTime;
+
+@Service
+public class CustomerLeadService {
+
+    private final CustomerRepository customerRepository;
+    private final LeadRepository leadRepository;
+    private final CustomerLeadRepository customerLeadRepository;
+
+    public CustomerLeadService(CustomerRepository customerRepository,
+                               LeadRepository leadRepository,
+                               CustomerLeadRepository customerLeadRepository) {
+        this.customerRepository = customerRepository;
+        this.leadRepository = leadRepository;
+        this.customerLeadRepository = customerLeadRepository;
+    }
+
+    @Transactional
+    public CustomerLead vincularLeadAoCustomer(Long customerId, Long leadId) {
+
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new RuntimeException("Customer não encontrado"));
+
+        Lead lead = leadRepository.findById(leadId)
+                .orElseThrow(() -> new RuntimeException("Lead não encontrado"));
+
+        CustomerLeadId id = new CustomerLeadId(customerId, leadId);
+
+        if (customerLeadRepository.existsById(id)) {
+            throw new RuntimeException("Esse lead já foi enviado para esse customer");
+        }
+
+        CustomerLead customerLead = new CustomerLead();
+        customerLead.setId(id);
+        customerLead.setCustomer(customer);
+        customerLead.setLead(lead);
+        customerLead.setDataEnvio(LocalDateTime.now());
+
+        return customerLeadRepository.save(customerLead);
+    }
+}
