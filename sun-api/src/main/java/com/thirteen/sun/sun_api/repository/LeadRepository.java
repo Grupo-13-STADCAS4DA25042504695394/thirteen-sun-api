5117f439466a101dfc84175f00b298f8aba11a2c
Daniel Berg <daniel.berg031@gmail.com>
2026-03-03 20:47:40 -0300

    chore: add filter

--- sun-api/src/main/java/com/thirteen/sun/sun_api/repository/LeadRepository.java
+++ sun-api/src/main/java/com/thirteen/sun/sun_api/repository/LeadRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.thirteen.sun.sun_api.models.Lead;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LeadRepository extends JpaRepository<Lead, Long> {
     Optional<Lead> findByEmail(String email);
+
+    List<Lead> findByPerfilDeConsumo(Integer perfilDeConsumo);
 }
